363690240f93ce703193767d7c4ead3578cb74a1
shivujagga <shivansh.jagga@gmail.com>
2017-04-04 01:35:27 +0530

    Point class
    Class created to store LATITUDE and LONGITUDE

---
+++ point.py
@@ -0,0 +1,6 @@
+class Point:
+    def __init__(self, latit_, longit_):
+    #def __init__( latit_, longit_):
+        #self.id = id_   #an id which uniquely identifies a point
+        self.latit = latit_
+        self.longit = longit_
